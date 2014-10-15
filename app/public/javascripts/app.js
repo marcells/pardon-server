b0666206d0793531c47210a005a43928b0482cd3
Marcell Spies <marcells@gmx.de>
2014-10-16 01:54:59 +0200

    Switched from jQuery to Angular.

--- app/public/javascripts/app.js
+++ app/public/javascripts/app.js
@@ -1,16 +1,41 @@
-$(function() {
-    var socket = io.connect('http://localhost:3000');
-        socket.on('recieve', function (data) {
-            console.log(data);
+
+var pardonApp = angular.module('pardonApp', []);
+
+pardonApp.directive('pardonEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.ctrlKey && event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.pardonEnter);
+                });
+
+                event.preventDefault();
+            }
         });
+    };
+});
 
-    var message = $('#message');
+pardonApp.controller('ChatController', function ($scope) {
+    var that = this;
 
-    message.keydown(function(event) {
-        if(event.keyCode === 13) {
-            socket.emit('send', { message: message.val() });
-            message.val('');
-            return false;
-        }
+    var socket = io.connect('http://localhost:3000');
+    
+    socket.on('recieve', function (data) {
+        $scope.$apply(function () {
+            that.messages.push({ 
+                user : 'foreign', 
+                text : data.message
+            });
+        });
     });
+
+    that.messages = [
+        {'user': 'User 1', 'text': 'Lorem impsum...'},
+        {'user': 'User 2', 'text': 'Lorem impsum dolor?'}
+    ];
+
+    $scope.send = function() {
+        socket.emit('send', { message: that.message });
+        that.message = '';
+    };
 });
